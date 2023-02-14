ba36ba04c5412dc791c7b9bd7dfd3e9f38c8745b
Chase Smith <ThaZeDi@gmail.com>
2023-02-14 13:50:39 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+function generatePassword() {
+  let password = "";
+  for (let i = 0; i < 15; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  document.getElementById("pbox1").innerHTML = password;
+
+  password = "";
+  for (let i = 0; i < 15; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  document.getElementById("pbox2").innerHTML = password;
+}
+
+function copyText1() {
+  let copyText = document.getElementById("pbox1");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+}
